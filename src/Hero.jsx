4b4fe2e8cb7c8438c5fbfa42fab67941817584f5
Rachele-Trimboli <146757214+Rachele-Trimboli@users.noreply.github.com>
2024-01-30 16:10:29 +0100

    img hero

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -3,6 +3,7 @@ const Hero = function () {
     <img
       src="https://www.libraccio.it/speciali/il-maggio-dei-libri-2019/il_maggio_dei_libri_sconto15.jpg"
       alt="promo"
+      style={{ width: "100%" }}
     />
   );
 };
